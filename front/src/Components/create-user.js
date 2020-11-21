933858e468b6581a82345bb79902819657e84a53
rahulkudum <rahulkudum@gmail.com>
2020-11-21 14:47:15 +0530

    trial mern

---
+++ front/src/Components/create-user.js
@@ -0,0 +1,55 @@
+import React, { useState,useEffect } from "react";
+import axios from "axios";
+
+
+function CreateUser(){
+const [username,setUsername]=useState("");
+
+
+
+
+function onsubmit(e){
+e.preventDefault();
+console.log(username);
+
+axios.post("http://localhost:5000/user/add",{username:username})
+.then(res=>console.log(res.data));
+
+setUsername("");
+
+
+
+
+
+
+
+}
+
+
+    return(
+        <div>
+        <p>hari bol</p>
+            <form onSubmit={onsubmit}>
+         
+ 
+  <div class="form-group ">
+  <label for="exampleInputEmail1">username</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" value={username} aria-describedby="emailHelp" onChange={(e)=>{
+        setUsername(e.target.value)
+    }}/>
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+
+</form>
+        </div>
+
+
+
+    );
+
+
+}
+
+
+export default CreateUser;
---
+++ front/src/Components/edit-exercise.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function EditExersies(){
+    return(<p>You are on Editing Exercises </p>);
+
+
+}
+
+export default EditExersies;
