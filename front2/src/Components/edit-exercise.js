35a54603aac3691c45c25af085bfb2d0f74656ef
rahulkudum <rahulkudum@gmail.com>
2020-11-21 23:20:34 +0530

    trial mern

--- front2/src/Components/edit-exercise.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function EditExersies(){
-    return(<p>You are on Editing Exercises </p>);
-
-
-}
-
-export default EditExersies;
--- front2/src/Components/exercise-list.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function ExerciseList(){
-    return(<p>You are on Exercuises list</p>);
-
-
-}
-
-export default ExerciseList;
