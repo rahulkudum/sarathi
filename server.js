19c3d330c7d7c9dcbd85df7663f08e85ecb8d9d2
rahulkudum <rahulkudum@gmail.com>
2020-12-21 11:45:56 +0530

    trial mern

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const app = express();
 
 var bodyParser = require("body-parser");
 
-app.use(bodyParser.json({ limit: "1000kb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 
 app.use(cors());
 
