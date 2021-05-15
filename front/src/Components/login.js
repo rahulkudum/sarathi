65e3d08e23319ae26694081e1f785aaf25004516
rahulkudum <rahulkudum@gmail.com>
2021-05-15 22:34:50 +0530

    demo

--- front/src/Components/login.js
+++ front/src/Components/login.js
@@ -93,7 +93,7 @@ function Login() {
        <p>To write {examname} please login through your G-mail</p>
       )}
       <GoogleLogin
-       clientId="526565895378-md97pueiv8m2t3c682eamv293tt4gaa6.apps.googleusercontent.com"
+       clientId="526565895378-erq5sbu7kkpeonhqgjvdemgtncbpep42.apps.googleusercontent.com"
        buttonText="Login through Gmail"
        onSuccess={(res) => {
         setBackdrop(true);
@@ -209,7 +209,7 @@ function Login() {
       <br />
       <p>To see your Dashboard please login through your G-mail </p>
       <GoogleLogin
-       clientId="526565895378-md97pueiv8m2t3c682eamv293tt4gaa6.apps.googleusercontent.com"
+       clientId="526565895378-erq5sbu7kkpeonhqgjvdemgtncbpep42.apps.googleusercontent.com"
        buttonText="Login through Gmail"
        onSuccess={(res) => {
         setBackdrop(true);
